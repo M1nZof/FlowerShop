f77bc677ec769157653eaedbca8381c87abea7c8
Oleg <olberd@bk.ru>
2023-08-22 22:09:12 +0300

    Добавил url, view на все страницы

--- flower_app/urls.py
+++ flower_app/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from flower_app import views
 
 urlpatterns = [
-    path('', views.index, name='main_page'),
+    path('', views.index, name='index'),
+    path('card', views.card, name='card'),
+    path('catalog', views.catalog, name='catalog'),
+    path('consultation', views.consultation, name='consultation'),
+    path('order_step', views.order_step, name='order_step'),
+    path('quiz', views.quiz, name='quiz'),
+    path('quiz_step', views.quiz_step, name='quiz_step'),
+    path('result', views.result, name='result'),
 
 ]
